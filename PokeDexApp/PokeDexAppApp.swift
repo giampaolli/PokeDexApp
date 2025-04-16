32fd81ad493e28eb38e6a1a7c47c25f3f2741adf
Felipe Giampaoli <giampaoli2011@gmail.com>
2025-04-16 14:43:50 -0300

    Initial Commit

---
+++ PokeDexApp/PokeDexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexAppApp.swift
+//  PokeDexApp
+//
+//  Created by Felipe Giampaoli on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
